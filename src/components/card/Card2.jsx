f95432c92d5b29a1ed9183658d60d7a73481c8b0
Vansh-0108 <vanshmahajan2001@gmail.com>
2024-02-16 16:13:24 +0530

    Change Class -> ClassName

--- src/components/card/Card2.jsx
+++ src/components/card/Card2.jsx
@@ -12,7 +12,7 @@ function Card2(props) {
                 <a href="#" className="text-[1rem] leading-6 text-white hover:text-[color:var(--main-color)]">{props.skills}</a>
                 <span className="text-[color:var(--main-color)] block text-[1.8rem] pt-0">{props.name}</span>
                 <p className="text-[1rem] text-[#ccc] leading-[1.8rem] px-0 py-0">{props.desc}</p>
-                <a href="#" class="btn pt-0 rounded-md">Read More</a>
+                <a href="#" className="btn pt-0 rounded-md">Read More</a>
             </div>
         </div>
     );
